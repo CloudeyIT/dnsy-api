262635408ad217215c0b41c72baa15d49a7b3806
Kristo Lippur <kristo@elnino.tech>
2023-03-03 14:38:00 +0100

    Add Sentry

--- Dnsy.Api/Program.cs
+++ Dnsy.Api/Program.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Sentry.Serilog;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
 
 namespace Dnsy.Api
 {
@@ -18,6 +22,33 @@ namespace Dnsy.Api
 
         public static IHostBuilder CreateHostBuilder (string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(
+                    (_, builder) =>
+                    {
+                        builder.AddEnvironmentVariables("APP__");
+                    })
+                .UseSerilog(
+                    (_, configuration) => configuration
+                        .Enrich.FromLogContext()
+                        .Enrich.WithExceptionDetails()
+                        .Enrich.WithEnvironmentName()
+                        .Enrich.WithMachineName()
+                        .WriteTo.Console(_.HostingEnvironment.IsProduction() ? LogEventLevel.Information : LogEventLevel.Debug)
+                        .WriteTo.Debug(_.HostingEnvironment.IsProduction() ? LogEventLevel.Information : LogEventLevel.Debug)
+                        .WriteTo.Sentry(
+                            sentry =>
+                            {
+                                var sentryConfig = _.Configuration.GetSection("Sentry").Get<SentrySerilogOptions>();
+                                if (sentryConfig?.Dsn is null) return;
+
+                                sentry.Dsn = sentryConfig.Dsn;
+                                sentry.TracesSampleRate = sentryConfig.TracesSampleRate;
+                                sentry.AttachStacktrace = true;
+                                sentry.InitializeSdk = true;
+                                sentry.AutoSessionTracking = true;
+                            }
+                        )
+                )
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
